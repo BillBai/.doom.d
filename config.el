baba8ad3dbdd3c4d6d3e0aca94ed3a74240828ab
billbai <billbai@tencent.com>
2020-01-19 17:07:34 +0800

    disable dap for now.

--- config.el
+++ config.el
@@ -109,13 +109,13 @@
 ;;
 ;; dap
 ;;
-(after! dap-mode
-  (dap-mode 1)
-  (dap-ui-mode 1)
-  ;; enables mouse hover support
-  (dap-tooltip-mode 1)
-  ;; use tooltips for mouse hover
-  ;; if it is not enabled `dap-mode' will use the minibuffer.
-  (tooltip-mode 1)
-  (require 'dap-gdb-lldb)
-  )
+;; (after! dap-mode
+;;   (dap-mode 1)
+;;   (dap-ui-mode 1)
+;;   ;; enables mouse hover support
+;;   (dap-tooltip-mode 1)
+;;   ;; use tooltips for mouse hover
+;;   ;; if it is not enabled `dap-mode' will use the minibuffer.
+;;   (tooltip-mode 1)
+;;   (require 'dap-gdb-lldb)
+;;   )
